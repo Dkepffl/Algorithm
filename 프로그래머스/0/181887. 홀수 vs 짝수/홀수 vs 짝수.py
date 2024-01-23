3d1339dd761fe624e122cccf6de3a442b0f3a8ef
Dkepffl <97338482+Dkepffl@users.noreply.github.com>
2024-01-23 12:43:00 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -0,0 +1,7 @@
+def solution(num_list):
+    even,odd=0,0
+    for i in range(0,len(num_list),2):
+        odd+=num_list[i]
+    for i in range(1,len(num_list),2):
+        even+=num_list[i]
+    return max(even,odd)
